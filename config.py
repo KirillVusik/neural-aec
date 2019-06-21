d1e43e7d8d6b788264dd487f496d4e841cfd906a
Kirill Vusik <kirillvusik@coherentsolutions.com>
2019-06-21 11:00:21 +0300

    Added pre-trained model

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-MODEL_ARCHITECTURE = 'todo.json'
-MODEL_WEIGHTS = 'todo.hdf5'
+MODEL_ARCHITECTURE = './trained_models/LSTM_2-layers-20-units-encoder_1-layers-20-units-decoder-cpu.json'
+MODEL_WEIGHTS = './trained_models/LSTM_2-layers-20-units-encoder_1-layers-20-units-decoder-gpu-006-0.998351-0.999620.weights.hdf5'
 
