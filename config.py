773699e890ffc555c3eeda7fd611188d9ec5651c
Kirill Vusik <kirillvusik@coherentsolutions.com>
2019-06-29 21:57:06 +0300

    GRU model with 2-layers encoder and decoder

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-MODEL_CHECKPOINT = './trained_models/LSTM_2-layers-100-units-encoder_2-layers-100-units-decoder-008-0.987870-0.988443.checkpoint.hdf5'
+MODEL_CHECKPOINT = './trained_models/GRU_2-layers-100-units-encoder_2-layers-100-units-decoder-047-0.991079-0.990958.checkpoint.hdf5'
